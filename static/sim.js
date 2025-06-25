1b8074dbacde8ca253f875eaae18517f1800528b
Emman Mlmb <emmanjibi@gmail.com>
2025-06-25 18:30:00 +0100

    Update sim.js

--- static/sim.js
+++ static/sim.js
@@ -12,3 +12,28 @@ socket.on("log_update", (msg) => {
   item.textContent = msg;
   document.getElementById("log").appendChild(item);
 });
+
+// Réception d'une bulle animée
+socket.on("bubble", (msg) => {
+  animateBubble(msg);
+});
+
+// Animation DOM
+function animateBubble(content = "msg") {
+  const bubble = document.getElementById("bubble");
+  bubble.innerText = content;
+  bubble.style.display = "block";
+  bubble.style.left = "0px";
+
+  let pos = 0;
+  const interval = setInterval(() => {
+    pos += 5;
+    bubble.style.left = `${pos}px`;
+    if (pos >= 350) {
+      clearInterval(interval);
+      setTimeout(() => {
+        bubble.style.display = "none";
+      }, 500);
+    }
+  }, 30);
+}
